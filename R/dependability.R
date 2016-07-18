c4918a03b782e0a93e6d7d7ec5e02d653aad818c
gela8 <gtl7@nau.edu>
2016-07-18 16:27:04 -0400

    made output table for subkoviak decision consistency function

--- R/dependability.R
+++ R/dependability.R
@@ -1,9 +1,9 @@
-data2 <- readRDS('data/brown-T_5_1a.rda')
+# data2 <- readRDS('data/brown-T_5_1a.rda')
 
-agree_tab <- readRDS('data/subkoviak-agreement-coefficient.rds')
-kappa_tab <- readRDS('data/subkoviak-kappa-coefficient.rds')
+# agree_tab <- readRDS('data/subkoviak-agreement-coefficient.rds')
+# kappa_tab <- readRDS('data/subkoviak-kappa-coefficient.rds')
 
-saveRDS(kappa_tab, 'data/subkoviak-kappa-coefficient')
+# subkoviak_data <- lst(agree_tab, kappa_tab)
 
 subkoviak <- function(data, items, raw_cut_score){
   
@@ -15,9 +15,27 @@ subkoviak <- function(data, items, raw_cut_score){
   
   z <- (c - .5 - M) / (S)
   
+  z_round_2 <- round(z, digits = 2)
+  
   z_rounded <- round(z, digits = 1)
   
-  rel <- round(alpha(data[items], check.keys = FALSE)$total$std.alpha,2)
+  rel <- round(alpha(data[items], check.keys = FALSE, warnings = FALSE)$total$std.alpha,1) %>%
+    as.character()
   
-  return(z)
+  agree_coef <- subkoviak_data$agree_tab %>%
+    select(z, contains(rel)) %>%
+    filter(z %in% z_rounded) %>%
+    .[[2]] %>%
+    round(., 2)
+  
+  kappa_coef <- subkoviak_data$kappa_tab %>%
+    select(z, contains(rel)) %>%
+    filter(z %in% z_rounded) %>%
+    .[[2]] %>%
+    round(., 2)
+    
+  
+  subkoviak_consistency <- c('z' = z_round_2, 'z_rounded' = z_rounded, 
+                             'agree_coef' = agree_coef, 'kappa_coef' = kappa_coef) 
+  return(subkoviak_consistency)
   }
