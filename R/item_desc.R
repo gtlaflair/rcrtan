56af7df8f757a19e396ac3e02875a20652b03947
gela8 <gtl7@nau.edu>
2016-07-18 14:45:16 -0400

    added agreement statistic and item phi

--- R/item_desc.R
+++ R/item_desc.R
@@ -1,50 +1,136 @@
-data <- readRDS('data/brown-item-data.rds')
+
 
 library(plyr)
 library(dplyr)
 library(tidyr)
 
+# Item facility -------------------------------------------------------
 
-IFpass <- function(data, pass_score){
-  data$pass <- ifelse(data$Total >= pass_score, 'pass', 'fail')
+
+IFpass <- function(data, items, cut_score, scale){
+  
+  data$raw_total <- rowSums(data[items])
+  data$perc_total <- (rowSums(data[items]) / length(items)) * 100
+  
+  ifelse(scale == 'percent', data$total <- data$perc_total, data$total <- data$raw_total)
+  
+  data$pass <- ifelse(data$total >= cut_score, 'pass', 'fail')
   
   IF_pass <- data %>%
     filter(pass %in% 'pass') %>%
-    select(contains("Q")) %>%
-    colMeans()  
+    .[items] %>%
+    colMeans()
 
   return(IF_pass)
 }
 
-IFfail <- function(data, pass_score){
-  data$pass <- ifelse(data$Total >= pass_score, 'pass', 'fail')
 
+IFfail <- function(data, items, cut_score){
+  
+  data$raw_total <- rowSums(data[items])
+  data$perc_total <- (rowSums(data[items]) / length(items)) * 100
+  
+  ifelse(scale == 'percent', data$total <- data$perc_total, data$total <- data$raw_total)
+  
+  data$pass <- ifelse(data$total >= cut_score, 'pass', 'fail')
+  
   IF_fail <- data %>%
     filter(pass %in% 'fail') %>%
-    select(contains("Q")) %>%
+    .[items] %>%
     colMeans()
   
   return(IF_fail)
 }
 
-bindex <- function(data, pass_score){
-  data$pass <- ifelse(data$Total >= pass_score, 'pass', 'fail')
+# B-index ---------------------------------------------------
+
+bindex <- function(data, items, cut_score){
+  
+  data$raw_total <- rowSums(data[items])
+  data$perc_total <- (rowSums(data[items]) / length(items)) * 100
+  
+  ifelse(scale == 'percent', data$total <- data$perc_total, data$total <- data$raw_total)
+  
+  data$pass <- ifelse(data$total >= cut_score, 'pass', 'fail')
   
   IF_pass <- data %>%
     filter(pass %in% 'pass') %>%
-    select(contains("Q")) %>%
+    .[items] %>%
     colMeans()
   
   IF_fail <- data %>%
     filter(pass %in% 'fail') %>%
-    select(contains("Q")) %>%
+    .[items] %>%
     colMeans()
   
   return(IF_pass - IF_fail)
 }
 
-agree <- function(data, pass_score){
-data$pass <- ifelse(data$Total >= pass_score, 'pass', 'fail')
+# Agreement index --------------------------------------------
 
+agree_index <- function(data, items, cut_score){
+  
+  data$raw_total <- rowSums(data[items])
+  data$perc_total <- (rowSums(data[items]) / length(items)) * 100
+  
+  ifelse(scale == 'percent', data$total <- data$perc_total, data$total <- data$raw_total)
+  
+  data$pass <- ifelse(data$total >= cut_score, 'pass', 'fail')
+
+  PiT <- data %>%
+    filter(pass == 'pass') %>%
+    .[items] %>%
+    summarise_each(funs(sum)) %>%
+    .[] / length(data[[1]])
+
+  Qi <- data %>%
+    .[items] %>%
+    summarise_each(funs(counts=sum(. == 0,na.rm=TRUE))) %>%
+    .[] / length(data[[1]])
+
+  Pt <- data %>%
+    summarise(pass_prop = length(which(pass == 'pass'))/length(pass)) %>%
+    rep(.,length(items)) %>%
+    unlist
+
+  Agree <- (2*PiT) + Qi - Pt
+
+  return(Agree)
 }
 
+# Item Phi ---------------------------------------------
+
+item_phi <- function(data, items, cut_score){
+  
+  data$raw_total <- rowSums(data[items])
+  data$perc_total <- (rowSums(data[items]) / length(items)) * 100
+  
+  ifelse(scale == 'percent', data$total <- data$perc_total, data$total <- data$raw_total)
+  
+  data$pass <- ifelse(data$total >= cut_score, 'pass', 'fail')
+  
+  PiT <- data %>%
+    filter(pass == 'pass') %>%
+    .[items] %>%
+    summarise_each(funs(sum)) %>%
+    .[] / length(data[[1]])
+  
+  Qi <- data %>%
+    .[items] %>%
+    summarise_each(funs(counts=sum(. == 0,na.rm=TRUE))) %>%
+    .[]/length(data[[1]])
+  
+  Pi <- 1 - Qi
+  
+  Pt <- data %>%
+    summarise(pass_prop = length(which(pass == 'pass'))/length(pass)) %>%
+    rep(.,length(items)) %>%
+    unlist
+  
+  Qt <- 1 - Pt
+  
+  Phi <- (PiT - (Pi*Pt)) / (sqrt(Pi * Qi * Pt * Qt))
+  
+  return(Phi)
+  
+}
