4a9baf41971f6dfed2d75f98701c3f49124a2796
gela8 <gtl7@nau.edu>
2016-07-18 14:44:55 -0400

    added function for Subkoviak's single administration dependability

---
+++ R/dependability.R
@@ -0,0 +1,21 @@
+data2 <- readRDS('data/brown-T_5_1a.rda')
+
+agree_tab <- readRDS('data/subkoviak-agreement-coefficient.rds')
+kappa_tab <- readRDS('data/subkoviak-kappa-coefficient.rds')
+
+saveRDS(kappa_tab, 'data/subkoviak-kappa-coefficient')
+
+subkoviak <- function(data, items, raw_cut_score){
+  
+  c <- raw_cut_score
+  
+  M <- mean(rowSums(data[items]))
+  
+  S <- sd(rowSums(data[items]))
+  
+  z <- (c - .5 - M) / (S)
+  
+  z_rounded <- round(z, digits = 1)
+  
+  return(z)
+  }
