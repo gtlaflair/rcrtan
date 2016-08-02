c81443708f388a92690ba48b8790893558caef91
gela8 <gtl7@nau.edu>
2016-08-02 14:09:32 -0400

    updated files

--- R/data.R
+++ R/data.R
@@ -1,4 +1,27 @@
+#' Brown's (2002, p. 124) Table 4.8 Item analysis data (first 10 items only)
+#'
+#' A data set containing the scored responses to first 10 items of a
+#' test and the total scores for 15 people
+#' @format A data frame with 15 rows and 12 variables
 
-'subkoviak-agreement-coefficient.rds'
+'brown_item_T4-8.rda'
 
-'subkoviak-kappa-coefficient.rds'
+#' Brown's (2002, p. 153) Table 5.1a Data for 45 examinees on 30 test items
+#'
+#' A data set containing the 45 scored responses to 30 items on a
+#' test of Listening (1-10), Reading (11-20), and Grammar (20-30)
+#' 
+#' @format A data frame with 45 rows and 30 variables
+'brown_depend_T5-1a.rda'
+
+#' Subkoviak's (1988) table of approximate values of the agreement coefficient
+#'
+#' 
+#' @format A data frame with 21 rows and 10 columns
+'sub_agree_coef.rda'
+
+#' Subkoviak's (1988) table of approximate values of the kappa coefficient
+#'
+#' 
+#' @format A data frame with 21 rows and 10 columns
+'sub_kappa_coef.rda'
--- R/dependability.R
+++ R/dependability.R
@@ -1,4 +1,11 @@
-
+#' Calculate Subkoviak's (1988) single administration consistency indices
+#' 
+#' @param data A data frame of dichotomously scored test items
+#' @param items Raw column indices representing the test items
+#' @param raw_cut_score The raw cut-score for the test
+#' @return The \code{z_cut} score and the rounded \code{z_cut_rounded} score for the test and rounded values for the \code{agree_coef} (agreement) and \code{kappa_coef} (kappa) coefficients from Subkoviak's (1988) tables
+#' @return The \code{z_cut} score and the rounded \code{z_cut_rounded} score for the test and rounded values for the \code{agree_coef} (agreement) and \code{kappa_coef} (kappa) coefficients from Subkoviak's (1988) tables
+#' @return The \code{z_cut} score and the rounded \code{z_cut_rounded} score for the test and rounded values for the \code{agree_coef} (agreement) and \code{kappa_coef} (kappa) coefficients from Subkoviak's (1988) tables
 
 subkoviak <- function(data, items, raw_cut_score){
   
@@ -10,9 +17,9 @@ subkoviak <- function(data, items, raw_cut_score){
   
   z <- (c - .5 - M) / (S)
   
-  z_round_2 <- round(z, digits = 2)
+  z_cut <- round(z, digits = 2)
   
-  z_rounded <- round(z, digits = 1)
+  z_cut_rounded <- round(z, digits = 1)
   
   rel <- round(alpha(data[items], check.keys = FALSE, warnings = FALSE)$total$std.alpha,1) %>%
     as.character()
@@ -30,7 +37,7 @@ subkoviak <- function(data, items, raw_cut_score){
     round(., 2)
     
   
-  subkoviak_consistency <- c('z' = z_round_2, 'z_rounded' = z_rounded, 
+  subkoviak_consistency <- c('z' = Z_cut, 'z_rounded' = z_cut_rounded, 
                              'agree_coef' = agree_coef, 'kappa_coef' = kappa_coef) 
   return(subkoviak_consistency)
   }
