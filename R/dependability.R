da1fa366796b3142ceefbb42677a4fd91490fb1d
Geoff LaFlair <gtlaflair@gmail.com>
2018-04-02 14:17:32 -1000

    updated subkoviak

--- R/dependability.R
+++ R/dependability.R
@@ -27,28 +27,28 @@ subkoviak <- function(data, items, n_items = NULL, raw_cut_score, look_up = FALS
   
   c <- raw_cut_score 
   
-  if(length(items) < 2 & !is.null(n_items)){
+  if(length(items) == 1){
     M <- data %>%
       select(., items) %$%
       mean(.[[1]])
   
   }
   
-  if(length(items) > 1 & is.null(n_items)){
+  if(length(items) > 1){
     M <- data %>%
       select(., items) %>%
       by_row(., sum, .collate = 'rows', .to = 'total') %$%
       mean(total)
   }
   
-  if(length(items) < 2 & !is.null(n_items)){
+  if(length(items) == 1){
     S <- data %>%
       select(., items) %$%
       sd(.[[1]])
     
   }
   
-  if(length(items) > 1 & is.null(n_items)){
+  if(length(items) > 1){
     S <- data %>%
       select(., items) %>%
       by_row(., sum, .collate = 'rows', .to = 'total') %$%
@@ -62,7 +62,7 @@ subkoviak <- function(data, items, n_items = NULL, raw_cut_score, look_up = FALS
   z_cut_rounded <- ifelse(abs(z) <= 2.0, round(z, digits = 1), 2) %>%
     abs()
   
-  if(length(items) < 2 & !is.null(n_items)){
+  if(length(items) == 1 & n_items > 1){
     rel <- (n_items / (n_items - 1)) * (1 - ((M * (n_items - M)) / (n_items * (S^2))))
   }
   
